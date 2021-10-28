6b461fdaccc88768f134661eed6be54998c42908
JerioHou <houjie20@gmail.com>
2021-10-28 15:18:19 +0800

    1137. 第 N 个泰波那契数

---
+++ NthTribonacciNumber_test.go
@@ -0,0 +1,25 @@
+package leetcode_go
+
+import "testing"
+
+func tribonacci(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n < 2 {
+		return 1
+	}
+	p, q, r, s := 0, 0, 1, 1
+	for i := 3; i <= n; i++ {
+		p = q
+		q = r
+		r = s
+		s = p + q + r
+	}
+	return s
+}
+
+func TestTribonacci(t *testing.T) {
+	t.Log(tribonacci(9))
+
+}
