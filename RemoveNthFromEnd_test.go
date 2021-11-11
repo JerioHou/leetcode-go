b5ca16da76ca33427a17d62b4ee1776e04b10082
JerioHou <houjie20@gmail.com>
2021-11-11 16:41:27 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ RemoveNthFromEnd_test.go
@@ -0,0 +1,45 @@
+/**
+ * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：head = [1,2,3,4,5], n = 2
+ * 输出：[1,2,3,5]
+ * 示例 2：
+ *
+ * 输入：head = [1], n = 1
+ * 输出：[]
+ * 示例 3：
+ *
+ * 输入：head = [1,2], n = 1
+ * 输出：[1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+package leetcode_go
+
+type MyListNode struct {
+	Val  int
+	Next *MyListNode
+}
+
+func removeNthFromEnd(head *MyListNode, n int) *MyListNode {
+	// 哨兵节点
+	sentinel := &MyListNode{0, head}
+	fast, slow := head, sentinel
+
+	for i := 0; i < n; i++ {
+		fast = fast.Next
+	}
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return sentinel.Next
+}
